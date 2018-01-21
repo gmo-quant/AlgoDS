81c7915244e8d30e9dfef26472f1e19e91044816
Guangzhuan <quan.fin.mo@gmail.com>
2018-01-20 22:33:58 -0500

    roate(d), use a tmp space to implement the rotation

--- DataStructureJavaGeeksForGeeks/src/ds_array/Rotation.java
+++ DataStructureJavaGeeksForGeeks/src/ds_array/Rotation.java
@@ -3,10 +3,32 @@ package ds_array;
 
 import java.util.*;
 
+/**
+ * 
+ * @author Guangzhuan Mo
+ * 
+ * rotate an array A[n] by d elements
+ * 
+ *  ex. 
+ *  Input:
+ *  	A[] 4, 2, 5, 7, 4, 6
+ *  	d = 2
+ *  Output:
+ *  	A[] 5, 7, 4, 6, 4, 2
+ *  
+ *
+ */
 public class Rotation {
 	private int[] A;
 	private Random rand;
-	
+
+	/*
+	 * initialize the A[N] with random numbers
+	 * 
+	 * @param N the size of the array A[]
+	 * 
+	 */
+
 	Rotation(int N){
 		A = new int[N];
 		rand = new Random();
@@ -15,6 +37,35 @@ public class Rotation {
 		}
 	}
 	
+	/*
+	 * use tmp space
+	 * 
+	 * 1. store the elements needed to rotate into tmp[d] 
+	 * 2. move the each element of A[d, n-1] to the left.
+	 * 3. restore tmp[] to the tail of A[n-d, n-1]
+	 */
+	void rotate(int d) {
+		int tmp[] = new int[d];
+		int i = 0;
+		// store the elements needed to rotate into tmp[d]
+		for (i = 0; i< d; i++) {
+			tmp[i] = A[i];
+		}
+		//move the each element of A[d, n-1] to the left.
+		for (i = d; i < A.length; i++) {
+			A[i-d] = A[i];
+		}
+		// restore tmp[] to the tail of A[n-d, n-1]
+		for (i = 0; i < d; i++) {
+			A[A.length-d+i] = tmp[i];
+		}
+	}
+	
+	
+	/*
+	 * display the result 
+	 * @param s the prompt info
+	 */
 	void display(String s) {
 		System.out.println(s);
 		for (int n : A) {
@@ -24,6 +75,9 @@ public class Rotation {
 	}
 	
 	public static void main(String[] args) {
-		
+		Rotation r = new Rotation(10);
+		r.display("original");
+		r.rotate(2);
+		r.display("after rotated 2 elements");
 	}
 }
