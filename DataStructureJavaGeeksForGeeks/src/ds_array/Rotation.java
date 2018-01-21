caa165dc92f83fe84d9acbf4bc49d07a485c21b1
Guangzhuan <quan.fin.mo@gmail.com>
2018-01-20 21:41:43 -0500

    initalize project

---
+++ DataStructureJavaGeeksForGeeks/src/ds_array/Rotation.java
@@ -0,0 +1,25 @@
+package ds_array;
+
+
+import java.util.*;
+
+public class Rotation {
+	private int[] A;
+	private Random rand;
+	
+	Rotation(int N){
+		A = new int[N];
+		rand = new Random();
+		for (int i = 0; i < N; i++) {
+			A[i] = rand.nextInt(50);
+		}
+	}
+	
+	void display(String s) {
+		System.out.println(s);
+		for (int n : A) {
+			System.out.print(n + ", ");
+		}
+		System.out.println("\n");
+	}
+}
